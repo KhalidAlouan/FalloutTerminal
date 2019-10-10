6c01f393f859f6236fadb1b22276d50cbb993db0
kalouan@iesesteveterradas.cat <kalouan@iesesteveterradas.cat>
2019-10-10 19:35:24 +0200

    Palabras

--- fterminal.php
+++ fterminal.php
@@ -3,16 +3,30 @@
 <head>
 	<title>Fallout Terminal</title>
 	<meta charset="utf-8">
+	<style type="text/css" src=""></style>
+	<script type="text/javascript" src="fterminalJS.js"></script>
 </head>
 <body>
 	<?php
-
-
-
-
-
-
-
+		$array= array();
+		$fp = fopen("paraules.txt", "r");
+		while (!feof($fp)){
+			$linea = fgets($fp);
+    		
+    	
+    		array_push($array, $linea);
+		}
+		fclose($fp);
+		
+		$randomPalabra = array_rand($array, 6);
+		echo "<div align='center'>";
+		echo $array[$randomPalabra[0]];
+		echo $array[$randomPalabra[1]];
+		echo $array[$randomPalabra[2]];
+		echo $array[$randomPalabra[3]];
+		echo $array[$randomPalabra[4]];
+		echo $array[$randomPalabra[5]];
+		echo "</div>";
 
 
 	?>
