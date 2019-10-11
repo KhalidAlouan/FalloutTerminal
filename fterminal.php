d8e4b76ca58e2a2907649efd43f3e3a8e257451e
PacoCS <pacocayuela13@gmail.com>
2019-10-11 17:40:48 +0200

    Keywords in random position

--- fterminal.php
+++ fterminal.php
@@ -13,10 +13,11 @@
 		$special_characters = array('!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','>','=','?','@','[',']','^','_','`','{','|','~');
 		$volcado = "";
 
-		$i = 1;
+		//
 
 		// Caracteres especiales de forma random
-		for ($i; $i<=354;$i++){
+
+		for ($i=1; $i<=354;$i++){
 			$volcado = $volcado . $special_characters[array_rand($special_characters,1)];
 			
 		}
@@ -35,25 +36,67 @@
 		$randomPalabra = array_rand($array, 6);
 		echo '<div id="box1">';
 		echo '<div id="box2">';
-		 $primera=$array[$randomPalabra[0]];
-		 $segunda=$array[$randomPalabra[1]];
-		 $terc=$array[$randomPalabra[2]];
-		 $cuarta=$array[$randomPalabra[3]];
-		 $quinta=$array[$randomPalabra[4]];
-		 $sexta=$array[$randomPalabra[5]];
+		//$primera=$array[$randomPalabra[0]];
+		//$segunda=$array[$randomPalabra[1]];
+		//$terc=$array[$randomPalabra[2]];
+		//$cuarta=$array[$randomPalabra[3]];
+		//$quinta=$array[$randomPalabra[4]];
+		//$sexta=$array[$randomPalabra[5]];
 
-		$special_characters = array('!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','>','=','?','@','[',']','^','_','`','{','|','~');
+		$volcado = $volcado;
+		$id_span = array('wfirst','wsecond','wthird','wfourth','wfiveth','wsixth');
+		$rand_pos = array('');
+		for ($i=0; $i < 6; $i++) { 
+			$random_position = rand(0,strlen($volcado)-1);
 
-		$volcado = "";
-
-		$i = 0;
-		for ($i; $i<=12;$i++){
-			$volcado = $volcado . $special_characters[array_rand($special_characters,1)];
-			
+			if (in_array($random_position, $rand_pos)) {
+				$i = $i - 1;
+				
+			}else{
+				$volcado = substr_replace($volcado, "<span>".$array[$randomPalabra[$i]]."</span>", $random_position, 0);
+				
+				array_push($rand_pos, $random_position);
+				array_push($rand_pos, $random_position-1);
+				array_push($rand_pos, $random_position+1);
+				
+			}
 		}
+			
 		
+
+
+		
+
+
+		// for ($i=0; $i < 6; $i++) { 
+		// 	$rand_pos = array('');
+		// 	$random_position = rand(0,strlen($volcado)-1);
+		// 	if (in_array($random_position, $rand_pos, false)) {
+		// 		$volcado = $volcado=$array[$randomPalabra[$i]];
+		// 	}
+		// }		
+			
+
+
+
+
+		
+		
+
+		// $special_characters = array('!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','>','=','?','@','[',']','^','_','`','{','|','~');
+
+		// $volcado = "";
+
+		// $i = 0;
+		// for ($i; $i<=12;$i++){
+		// 	$volcado = $volcado . $special_characters[array_rand($special_characters,1)];
+			
+		// }
+		
+
 		$volcado=preg_replace('/\s+/','',$volcado);
-		echo $volcado.$array[array_rand($array,1)];
+		// echo $volcado.$array[array_rand($array,1)];
+		echo $volcado;
 		
 		echo "</div>";
 		echo "</div>";
