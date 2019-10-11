f33a1ade724d78d4b23a1d3141b335d253c51ede
computing-student <informatica.s.132@gmail.com>
2019-10-11 16:50:31 +0200

    Commit table php

--- fterminal.php
+++ fterminal.php
@@ -9,17 +9,24 @@
 </head>
 <body>
 	<?php
+		// Array que contindrà totes les paraules.
 		$array= array();
+		// Obrim l'arxiu que conté les paraules.
 		$fp = fopen("paraules.txt", "r");
+		
+		// Emplenem l'array.
 		while (!feof($fp)){
 			$linea = fgets($fp);
-    		
-    	
     		array_push($array, $linea);
 		}
+
+		// Tanquem l'arxiu.
 		fclose($fp);
-		
+
+		// Escollim sis paraules a l'atzar.
 		$randomPalabra = array_rand($array, 6);
+
+		// Mostrem les paraules per pantalla.
 		echo '<div id="box1">';
 		echo '<div id="box2">';
 		echo $array[$randomPalabra[0]];
@@ -28,9 +35,42 @@
 		echo $array[$randomPalabra[3]];
 		echo $array[$randomPalabra[4]];
 		echo $array[$randomPalabra[5]];
+
+		$arrayDirecciones1 = array("0xF91C", "0xF928", "0xF934", "0xF940", "0xF94C", "0xF958", "0xF964", "0xF970", 
+									"0xF97C", "0xF988", "0xF994", "0xF9A0", "0xF9AC", "0xF9B8", "0xF9C4", "0xF9D0", "0xF9DC");
+
+		$arrayDirecciones2 = array("0xF9E8", "0xF9F4", "0xFA00", "0xFAFC", "0xFA18", "0xFA24", "0xFA30", "0xFA3C", 
+									"0xFA48", "0xFA54", "0xFA60", "0xFA6C", "0xF978", "0xFA84", "0xFA90", "0xFA9C", "0xFAA8");
+
+		// I create the table.
+		echo '<table id="table1">';
+		echo '<tbody>';
+
+		// I create every row iterating.
+		for($i=0; $i < 17; $i++) {
+
+			echo '<tr>';
+
+			$direction1 = $arrayDirecciones1[$i];
+			$direction2 = $arrayDirecciones2[$i];
+
+			echo '<th class="column1">';
+			echo "$direction1</th>";
+			echo '<th class="column2"></th>';
+			echo '<th class="column1">';
+			echo "$direction2</th>";
+			echo '<th class="column2"></th>';
+
+			echo '</tr>';
+		}
+
+		echo '</tbody>';
+		echo '</table>';
+
 		echo "</div>";
 		echo "</div>";
 
 	?>
+
 </body>
 </html>
