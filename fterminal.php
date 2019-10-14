ac8d4e3cc2a60151cfe003e4b40bbd31f2ea631b
kalouan@iesesteveterradas.cat <kalouan@iesesteveterradas.cat>
2019-10-14 17:18:50 +0200

    P

--- fterminal.php
+++ fterminal.php
@@ -46,15 +46,7 @@
 		echo '<div id="box1">';
 		echo '<div id="box2">';
 
-<<<<<<< HEAD
-		echo $array[$randomPalabra[0]];
-		echo $array[$randomPalabra[1]];
-		echo $array[$randomPalabra[2]];
-		echo $array[$randomPalabra[3]];
-		echo $array[$randomPalabra[4]];
-		echo $array[$randomPalabra[5]];
-		echo '<br>';
-=======
+
 
 		//$primera=$array[$randomPalabra[0]];
 		//$segunda=$array[$randomPalabra[1]];
@@ -62,7 +54,7 @@
 		//$cuarta=$array[$randomPalabra[3]];
 		//$quinta=$array[$randomPalabra[4]];
 		//$sexta=$array[$randomPalabra[5]];
->>>>>>> 8fd43c6e6a280723bf0bcda720c8773570a56b57
+
 
 		$arrayDirecciones1 = array("0xF91C", "0xF928", "0xF934", "0xF940", "0xF94C", "0xF958", "0xF964", "0xF970", 
 									"0xF97C", "0xF988", "0xF994", "0xF9A0", "0xF9AC", "0xF9B8", "0xF9C4", "0xF9D0", "0xF9DC");
@@ -120,7 +112,7 @@
 				
 				array_push($rand_pos, $random_position);
 				array_push($rand_pos, $random_position-1);
-				array_push($rand_pos, $random_position+1);
+				array_push($rand_pos, $random_position+2);
 				
 			}
 		}
@@ -130,11 +122,11 @@
 
 		
 
-<<<<<<< HEAD
+
 		$i = 1;
 		for ($i; $i<=12;$i++){
 			$volcado = $volcado . $special_characters[array_rand($special_characters,1)];
-=======
+		}
 
 		// for ($i=0; $i < 6; $i++) { 
 		// 	$rand_pos = array('');
@@ -143,7 +135,7 @@
 		// 		$volcado = $volcado=$array[$randomPalabra[$i]];
 		// 	}
 		// }		
->>>>>>> 8fd43c6e6a280723bf0bcda720c8773570a56b57
+
 			
 
 
