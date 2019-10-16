1ba2ef8c81cb089dfadfc5fd6d2a4ea4ed372db1
computing-student <informatica.s.132@gmail.com>
2019-10-16 19:24:44 +0200

    Commit spans onclick

--- fterminal.php
+++ fterminal.php
@@ -121,7 +121,7 @@
 		echo '<table id="table1">';
 		echo '<tbody>';
 
-
+		//echo "<p>$volcado</p>";
 
 
 		//Llistes amb el volcat a pujar.
@@ -159,7 +159,7 @@
 				if (strpos($tros, $lletra) && $seguir == true) {
 					$pos_lletra = intval(strpos($tros, $lletra));
 					$tros2 = substr_replace($tros, '</span>', $pos_lletra+5);
-					$tros3 = substr_replace($tros2, '<span>'.$choosen_word_array[$num_par], $pos_lletra);
+					$tros3 = substr_replace($tros2, '<span onclick="wordSelected(); comprovar();">'.$choosen_word_array[$num_par], $pos_lletra);
 					$num_par += 1;
 					$seguir = false;
 					$substitucio = true;
