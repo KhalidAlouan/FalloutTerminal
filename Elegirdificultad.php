4e468d179ee0368001987acaceeb989fcc20cfa7
KhalidAlouan <kalouan@iesesteveterradas.cat>
2019-11-07 13:00:17 +0100

    Dificultad

--- Elegirdificultad.php
+++ Elegirdificultad.php
@@ -8,7 +8,7 @@
 <body>
 
 	<div id="options">
-	<h1 >Choose the dificult:</h1>
+	<h1 >Choose the difficulty:</h1>
 		<input class="butn" type="submit" name="play" value="Normal" onclick="window.location.href='./fterminal.php'">
 		<input class="butn" type="submit" name="seeRanking" value="Dificil" onclick="window.location.href='./Hard/fterminalHARD.php'">
 		<input class="butn" type="submit" name="seeRanking" value="Extremo" onclick="window.location.href='./Extrem/fterminalExtrem.php'">
